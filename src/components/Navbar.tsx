0b1405ba8b97548e1973f00cd79507eae1cf474d
Dana Sharon <whosedreamisthis@gmail.com>
2026-04-27 14:24:34 -0700

    logo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => (
+	<nav className="navbar">
+		<div className="brand">
+			<div className="mark">
+				<div className="glyph" />
+			</div>
+		</div>
+	</nav>
+);
+
+export default Navbar;
